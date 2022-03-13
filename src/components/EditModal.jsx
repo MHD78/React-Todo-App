fb8e1a752e2531235cff1acad66b340600b4878a
mhd78m <mdamirchi27@gmail.com>
2022-03-13 17:47:03 +0330

    Edit and Extra Modals

---
+++ src/components/EditModal.jsx
@@ -0,0 +1,28 @@
+import { useState, useRef, useEffect } from "react";
+import { BiX } from "react-icons/bi";
+
+const EditModal = ({ values, edit, setEdit, onEdit }) => {
+
+    const [value, setValue] = useState("");
+    const inp = useRef();
+    useEffect(() => {
+        inp.current.focus();
+    }, [edit]);
+
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        onEdit(values.todo.id, value)
+        setEdit(false);
+        setValue("");
+    }
+    return (
+        <form onSubmit={submitHandler} className={`justify-center w-full h-full items-center gap-x-2  right-0 top-0 bg-black/80 rounded  absolute ${edit ? "flex" : "hidden"}`} >
+            <BiX onClick={() => setEdit(false)} className="text-xl  text-white cursor-pointer absolute top-1 right-1" />
+            <input ref={inp} required onChange={e => setValue(e.target.value)} value={value} type={"text"} className="px-2 w-2/4 py-1 rounded outline-slate-500" />
+            <button type="submit" className="px-4 py-1 sm:px-8 rounded text-white bg-slate-600 hover:bg-slate-700 " >Edit</button>
+        </form>
+    );
+}
+
+export default EditModal;
---
+++ src/components/ExtraModal.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import { BiCommentEdit, BiX, BiTrash } from "react-icons/bi";
+
+
+
+const ExtraModal = ({ modal, setModal, setEdit, values }) => {
+
+
+    return (
+        <div className={`flex-col bg-gray-100 absolute top-1 right-4 w-20 rounded-lg p-1 m-1  ${modal ? " flex" : "hidden"}`} >
+            <BiX onClick={() => setModal(false)} className="text-lg self-end cursor-pointer" />
+            <span  >
+                <span onClick={() => { setEdit(true); setModal(false) }} className={`flex justify-start items-center gap-x-1 cursor-pointer transition duration-300 rounded-lg p-0.5 ${values.isComplete ? "text-slate-400 " : "hover:text-white hover:bg-sky-600"} `}  >
+                    <BiCommentEdit className="text-lg" />
+                    Edit
+                </span>
+                <span onClick={() => values.onDelete(values.todo.id)} className={`flex justify-start items-center gap-x-1 cursor-pointer transition duration-300 rounded-lg p-0.5 ${values.isComplete ? "text-slate-400 " : "hover:text-white hover:bg-red-600"} `} >
+                    <BiTrash className="text-lg" />
+                    Delete
+                </span>
+            </span>
+
+        </div>
+    );
+}
+
+export default ExtraModal;
